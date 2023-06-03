5737bc9916e43b870ab1e67089fdf5d5b7602df1
gvsiva2008 <gvsiva2008@gmail.com>
2023-06-03 11:30:54 +0530

    java file created

---
+++ hello.java
@@ -0,0 +1,9 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, India");
+        System.out.println("Hello,");
+        System.out.println("Hello, Siva");		
+    }
+}
