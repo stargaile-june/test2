cdbb5d458ae8797217dcd03c06dc7538f440db72
gvsiva2008 <gvsiva2008@gmail.com>
2023-06-03 11:36:47 +0530

    java file removed

--- hello.java
+++
@@ -1,9 +0,0 @@
-// Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, India");
-        System.out.println("Hello,");
-        System.out.println("Hello, Siva");		
-    }
-}
